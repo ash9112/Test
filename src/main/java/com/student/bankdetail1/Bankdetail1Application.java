ca4655e5f4cd7c2baeec914c17687efbf19736d2
ash9112 <ashi.mishra9112@gmail.com>
2024-04-15 22:57:20 +0530

    yes

---
+++ src/main/java/com/student/bankdetail1/Bankdetail1Application.java
@@ -0,0 +1,13 @@
+package com.student.bankdetail1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bankdetail1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bankdetail1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/student/bankdetail1/Bankdetail1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.student.bankdetail1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bankdetail1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
